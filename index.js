e79d518da391329436ab548c09eb92b96a207644
Trinmar Boado <timi.boado@gmail.com>
2020-03-14 21:23:32 +0800

    First node app deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 // const trinDB = require('./TrinDB.js')
 const trinDB = require('trin.db')
 const app = express()
-const port = 8081
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
